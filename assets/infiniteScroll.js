3c355cd3c656baf383a73ef3cb199057ee7ee93e
danilovict2 <danilosindric@gmail.com>
2023-09-23 17:39:20 +0200

    minor changes

--- assets/infiniteScroll.js
+++ assets/infiniteScroll.js
@@ -1,8 +1,10 @@
 import InfiniteScroll from "infinite-scroll";
 
 const actorsGrid = document.querySelector('.popular-actors-grid');
-const infScroll = new InfiniteScroll(actorsGrid, {
-    path: '/actors/page/{{#}}',
-    append: '.actor',
-    status: '.page-load-status',
-});
+if (actorsGrid) {
+    const infScroll = new InfiniteScroll(actorsGrid, {
+        path: '/actors/page/{{#}}',
+        append: '.actor',
+        status: '.page-load-status',
+    });
+}
